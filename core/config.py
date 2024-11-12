675f18957fca85a4665eb351adadacc1aedd35fd
thkim <thkim96@ssrinc.co.kr>
2024-11-12 16:23:19 +0900

    DB값 Config.ini에서 가져오기

--- core/config.py
+++ core/config.py
@@ -1,18 +1,26 @@
 # coding=utf-8
 from dotenv import load_dotenv
+import os
+import configparser
+import ctypes
 
 load_dotenv()
 
 
 class DbSetting:
 
-    DB_USERNAME = "thadmin"
-    DB_PASSWORD = "thadmin123"
-    DB_HOST = "localhost"
-    DB_PORT = "5432"
-    DB_DATABASE = "thdb"
+    main_conf = configparser.ConfigParser()
+    main_conf.read("MainConfig.ini")
 
-    DATABASE_URL = f"postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_DATABASE}"
+    information = main_conf["information"]
+
+    database_id = information["database_id"]
+    database_password = information["database_password"]
+    database_ip = information["database_ip"]
+    database_port = information["database_port"]
+    database_name = information["database_name"]
+
+    DATABASE_URL = f"postgresql://{database_id}:{database_password}@{database_ip}:{database_port}/{database_name}"
 
 
 settings = DbSetting()
